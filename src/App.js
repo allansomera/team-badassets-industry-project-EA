1f0b60747f1be8266771ce2ec288580cdf536e30
Allan Somera <nagal.somera@gmail.com>
2022-12-02 01:30:27 -0800

    merged dev for PR

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Layout from "./components/layout/Layout";
 
 function App() {
   const [redeemModal, setredeemModal] = useState({
-    isActive: true,
+    isActive: false,
   });
   return (
     <>
