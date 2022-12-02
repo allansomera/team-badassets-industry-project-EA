30dbeae120445ee0755d07c6a497535a5b4ec004
Olanrewaju-Ak <akinola.lanrewaju@yahoo.com>
2022-12-01 19:03:31 -0800

    overview combined with barebones

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 import "./App.scss";
-<<<<<<< HEAD
-import OverView from "./components/overview/OverView";
-
-function App() {
-	return (
-		<div className="App">
-			<OverView />
-		</div>
-	);
-=======
 import Header from "./components/header/Header";
 import LeftSidebar from "./components/left-sidebar/LeftSidebar";
 import Main from "./components/main/Main";
 import { Routes, Route, Outlet } from "react-router-dom";
+import OverView from "./components/overview/OverView";
 
 const Layout = () => {
-  return (
-    <>
-      <Header />
-      <div className="container">
-        <LeftSidebar />
-        <Main />
-        <Outlet />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<div className="container">
+				<LeftSidebar />
+				<Main />
+				<Outlet />
+			</div>
+		</>
+	);
 };
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />} />
-      </Routes>
-    </>
-  );
->>>>>>> dev
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Layout />} />
+				<Route path="/overview" element={<OverView />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
