5dd6d0f6a5ebaff4cfcd5e5fbe4055020dd97275
Allan Somera <nagal.somera@gmail.com>
2022-12-01 18:25:13 -0800

    barebones main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import "./App.scss";
+import Header from "./components/header/Header";
+import LeftSidebar from "./components/left-sidebar/LeftSidebar";
+import Main from "./components/main/Main";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <LeftSidebar />
+        <Main />
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 function App() {
-	return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
