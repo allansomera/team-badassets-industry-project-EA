320198bdd78137890d2be68064b472cd530bf411
Lakpa Dietz <ldietz086@gmail.com>
2022-12-02 01:08:38 -0800

    Completed Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,33 @@
 import "./Header.scss";
 import React from "react";
+import Logo from "../../assets/logos/ea-logo.png";
+import Cart from "../../assets/icons/cart.png";
+import Bell from "../../assets/icons/bell.png";
+import Setting from "../../assets/icons/setting.png";
+import Account from "../../assets/icons/account.png";
+import DesktopIcons from "../../assets/icons/desktop-icons.png";
+import Online from "../../assets/icons/online.png"
 
 const Header = () => {
-  return <div className="header">EA</div>;
+  return (
+    <>
+      <div className="header__bar">
+        <img className="header__bar-icons" src={DesktopIcons}></img>
+      </div>
+      <header className="header">
+        <div>
+          <img className="header__logo-ea" src={Logo}></img>
+        </div>
+        <div className="header__logo-container">
+        <img className="header__logo" src={Cart}></img>
+        <img className="header__logo" src={Bell}></img>
+        <img className="header__logo" src={Setting}></img>
+        <img className="header__logo" src={Account}></img>
+        <img className="header__logo-online" src={Online}></img>
+      </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
